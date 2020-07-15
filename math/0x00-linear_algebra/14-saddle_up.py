eca57496186ee7e34dfdf33f82ace788f41536c9
Luis Chaparro <lfchaparro@outlook.com>
2020-07-14 22:22:41 -0500

    Adding a function that performs matrix multiplication

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" Multiplicates two matrices """
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Performs matrix multiplication
+
+    Returns:
+        Multiplicated matrices as a numpy.ndarray
+    """
+    return np.matmul(mat1, mat2)
