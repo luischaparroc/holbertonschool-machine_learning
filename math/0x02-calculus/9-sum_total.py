d98b9337458009f87c0b1183125e6b332ab45ffa
Luis Chaparro <lfchaparro@outlook.com>
2020-07-24 19:21:42 -0500

    Fixing summation function using Faulhaber's formula

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -2,21 +2,6 @@
 """Function that calculates a summation"""
 
 
-def calculate_summation(i, n):
-    """Function that calculates summation setting the series recursively
-
-    Args:
-        i: index (dummy variable)
-        n: limit
-
-    Returns:
-        Summation
-    """
-    if i > n:
-        return 0
-    return (i ** 2) + calculate_summation(i + 1, n)
-
-
 def summation_i_squared(n):
     """Function that returns the result of the summation
 
@@ -28,4 +13,4 @@ def summation_i_squared(n):
     """
     if type(n) is not int or n < 1:
         return None
-    return calculate_summation(1, n)
+    return (n*(n + 1) * (2*n + 1))//6
