9558cac05248f8c80456a400f65bdb7d9ab23867
Luis Chaparro <lfchaparro@outlook.com>
2020-07-29 22:12:25 -0500

    Adding probability mass function of Poisson class

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
-"""Module with class Poisson"""
+"""Module with Poisson class"""
 
 
 class Poisson:
     """Class that represents a Poisson distribution"""
+
+    EULER_NUMBER = 2.7182818285
 
     def __init__(self, data=None, lambtha=1.):
         """Constructor Poisson class
@@ -22,3 +24,33 @@ class Poisson:
             if lambtha <= 0:
                 raise ValueError('lambtha must be a positive value')
             self.lambtha = float(lambtha)
+
+    @staticmethod
+    def factorial(n):
+        """Calculates factorial of given number
+
+        Args:
+            n: input number
+
+        Returns:
+            response of n factorial
+        """
+        if n == 0:
+            return 1
+        return n * Poisson.factorial(n - 1)
+
+    def pmf(self, k):
+        """Calculates Probability Mass Function (PMF)
+
+        Args:
+            k: number of successes
+
+        Returns:
+            PMF of k or 0 if k is out of range.
+        """
+        k = int(k)
+        if k < 0:
+            return 0
+        num = (self.lambtha ** k) * (self.EULER_NUMBER ** -self.lambtha)
+        den = self.factorial(k)
+        return num/den
