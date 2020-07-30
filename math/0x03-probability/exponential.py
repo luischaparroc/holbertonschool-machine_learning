f6a279ebd9fc45cfa892a7465aad540f2fcc149a
Luis Chaparro <lfchaparro@outlook.com>
2020-07-29 22:51:50 -0500

    Adding Probability Density Function (PDF) into Exponential class

--- math/0x03-probability/exponential.py
+++ math/0x03-probability/exponential.py
@@ -5,6 +5,8 @@
 class Exponential:
     """Class that represents an exponential distribution"""
 
+    EULER_NUMBER = 2.7182818285
+
     def __init__(self, data=None, lambtha=1.):
         """Class constructor"""
         if data is not None:
@@ -17,3 +19,17 @@ class Exponential:
             if lambtha <= 0:
                 raise ValueError('lambtha must be a positive value')
             self.lambtha = float(lambtha)
+
+    def pdf(self, x):
+        """Calculates Probability Density Function (PDF)
+
+        Args:
+            x: time period
+
+        Returns:
+            PDF of x or 0 if x is out of range.
+        """
+        if x < 0:
+            return 0
+
+        return self.lambtha * (self.EULER_NUMBER ** (-self.lambtha * x))
