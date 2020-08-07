a543b3483abf882099acee8796f052bdbcf62434
Luis Chaparro <lfchaparro@outlook.com>
2020-08-07 16:49:47 -0500

    Adding DeepNeuralNetwork class

---
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""Deep Neural Network class"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """Class that defines a neural network with one hidden performing
+    binary classification
+    """
+
+    @staticmethod
+    def he_et_al(nx, layers):
+        """Calculates weights using he et al method"""
+        weights = dict()
+        for i in range(len(layers)):
+            if type(layers[i]) is not int or layers[i] < 1:
+                raise TypeError('layers must be a list of positive integers')
+            prev_layer = layers[i - 1] if i > 0 else nx
+            w_part1 = np.random.randn(layers[i], prev_layer)
+            w_part2 = np.sqrt(2 / prev_layer)
+            weights.update({
+                'b' + str(i + 1): np.zeros((layers[i], 1)),
+                'W' + str(i + 1): w_part1 * w_part2
+            })
+        return weights
+
+    def __init__(self, nx, layers):
+        """Class constructor"""
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if type(layers) is not list or len(layers) == 0:
+            raise TypeError('layers must be a list of positive integers')
+
+        self.L = len(layers)
+        self.cache = dict()
+        self.weights = self.he_et_al(nx, layers)
